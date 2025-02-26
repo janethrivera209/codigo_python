0ac618a340d46b6965ff5d752e2986d7eab820a0
janethrivera209 <riverarobledojaneth@gmail.com>
2025-02-25 21:16:09 -0600

    Add files via upload

---
+++ tablas de multiplicar.py
@@ -0,0 +1,11 @@
+i = 1
+while i <= 10:  
+    print(f"Tabla del {i}:")
+    j = 1
+    while j <= 10: 
+        resultado = i * j
+        print(f"{i} x {j} = {resultado}")
+        j += 1  
+    print()  
+    i += 1  
+    
